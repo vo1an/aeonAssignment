c18413ffe52b206d5693181595a42adb69e773dc
ngetvi <ngetvi@deakin.edu.au>
2022-07-05 21:41:41 +0700

    initial commit

---
+++ src/main/java/com/virakvoan/demo/PositionApplication.java
@@ -0,0 +1,13 @@
+package com.virakvoan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/virakvoan/demo/PositionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virakvoan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
