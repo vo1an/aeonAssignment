342a1851ed0780c85a4a73fa9af2248a2de2d8af
ngetvi <ngetvi@deakin.edu.au>
2022-07-06 15:37:14 +0700

    run only on port 8080

--- src/main/java/com/virakvoan/demo/cotroller/PositionController.java
+++ src/main/java/com/virakvoan/demo/cotroller/PositionController.java
@@ -1,5 +1,37 @@
 package com.virakvoan.demo.cotroller;
 
-public class PositionController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.virakvoan.demo.model.position;
+import com.virakvoan.demo.service.PositionService;
+
+@RestController
+@RequestMapping("/api")
+
+public class PositionController {
+	 @Autowired
+     PositionService posService;
+	 
+	 @RequestMapping(value="/positions", method=RequestMethod.POST)
+	 public position createPosition(@RequestBody position pos) {
+	     return posService.createposition(pos);
+	 }
+	 
+	 @RequestMapping(value="/positions", method=RequestMethod.GET)
+	 public List<position> readPositions() {
+	     return posService.getpositions();
+	 }
+
+	 @RequestMapping(value="/positions/{ID}", method=RequestMethod.DELETE)
+	 public void deleteEmployees(@PathVariable(value = "ID") Long id) {
+	     posService.deleteposition(id);
+	 }
 }
